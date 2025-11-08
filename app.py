013450a815ed2a9cdd6b8013d78fd23ff4a86875
NagarjunaVV <nagarjunavv761@gmail.com>
2025-11-08 13:01:22 +0530

    Shorten title of the movie recommender system

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ except Exception as e:
 
 # --- Streamlit Web App UI ---
 
-st.title('🎬 Indian Movie Recommender System')
+st.title('Indian Movie Recommender')
 st.markdown('Find movies similar to the one you like!')
 
 # Create a select box for movie selection
